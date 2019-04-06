4a915b9c829d16be301ebeef29757dbb30f1cf5a
yll <2647238171@qq.com>
2019-04-06 16:38:23 +0800

    第二次提交

--- src/main/java/com/yll/Hello.java
+++ src/main/java/com/yll/Hello.java
@@ -2,8 +2,6 @@ package com.yll;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("aaa");
-        System.out.println("bbb");
-
+        System.out.println("Hello");
     }
 }
